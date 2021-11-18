8e1243abc70f5088b83c7b8d10b0db14d560eec1
Caster <44622004+Casterrr@users.noreply.github.com>
2021-11-18 20:49:42 -0300

    Erro corrigido, desafio concluido

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -7,7 +7,7 @@ class TurnUserAdminController {
 
   handle(request: Request, response: Response): Response {
     // Complete aqui
-    const { user_id } = request.body;
+    const { user_id } = request.params;
 
     try {
       const user = this.turnUserAdminUseCase.execute({ user_id });
